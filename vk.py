9a4b895c692945fcfec5f2556dc265897a65741a
Oleg Burik <56690876+burik193@users.noreply.github.com>
2023-03-01 18:24:28 +0100

    Documentation added

--- vk.py
+++ vk.py
@@ -5,14 +5,14 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 
-token = '8e81b593e23497d1546f9b9dc45c0013f55ac67ec051a6ab15457eabe232f0f3ba6f4876790d868b2fe3e'
-version = 5.103
-domain = 'legion_damned'
-group_id = 120667630
+token = 'your_token'  # e.g.'8e81b593e23497d1546f9b9dc45c0013f55ac67ec051a6ab15457eabe232f0f3ba6f4876790d868b2fe3e'
+version = 'your_version'  # 5.103
+domain = 'your_domain'  #'legion_damned'
+group_id = 'your_group_id_vk'  # 120667630
 
 print('Start Bot')
 
-vk_session = vk_api.VkApi(token=token, scope=140488159)
+vk_session = vk_api.VkApi(token=token, scope='your_scope')   # e.g. 140488159
 
 session_api = vk_session.get_api()
 
